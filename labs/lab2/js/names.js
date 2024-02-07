f42f8b59a0452044bc06251c2176403f2d38125b
Michael Felix <mikemh@uri.edu>
2024-02-07 12:28:57 -0500

    Update names.js

--- labs/lab2/js/names.js
+++ labs/lab2/js/names.js
@@ -2,15 +2,15 @@ var hacker1 = "Michael"
 var hacker2 = "Connor"
 
 
-console.log(`My name is ${hacker1}`)
-console.log(`My partner's name is ${hacker2}`)
+console.log(`My name is ` + hacker1)
+console.log(`My partner's name is ` + hacker2)
 
 if (hacker1.length > hacker2.length) {
-    console.log(`I have the longest name, it has ${hacker1.length} characters!`)
+    console.log(`I have the longest name, it has ` + hacker1.length + ` characters!`)
 } else if (hacker1.length < hacker2.length) {
-    console.log(`It seems that my partner has the longest name, it has ${hacker2.length} characters!`)
+    console.log(`It seems that my partner has the longest name, it has ` + hacker2.length + ` characters!`)
 } else {
-    console.log(`Wow, we both have equally long names, ${hacker2.length} characters!`)
+    console.log(`Wow, we both have equally long names, ` + hacker2.length + ` characters!`)
 }
 
 for (let i = 0; i < hacker1.length; i++) {
