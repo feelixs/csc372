8ebc2d691814c116f933fa3c8a442222ade7fea8
Michael Felix <mikemh@uri.edu>
2024-02-20 10:35:52 -0500

    jquery css

--- inclass/jquery_practice/js/jquery-script.js
+++ inclass/jquery_practice/js/jquery-script.js
@@ -1,11 +1,23 @@
 let title = $('h1');
+title.className = 'main-title';
 
 title.html('<i>My jQuery Example<\/i>');
 
-title.append('<h1>jQuery is a JavaScript Library.<br>jQuery greatly simplifies JavaScript programming.<br>jQuery is easy to learn.<\/h1>');
+title.after('<h1>jQuery is a JavaScript Library.<br>jQuery greatly simplifies JavaScript programming.<br>jQuery is easy to learn.<\/h1>');
 
 
 title.append('<ul><\/ul>')
 $('ul').append('<li>HTML/DOM manipulation<\/li>').append('<li>CSS manipulation<\/li>').append('<li>HTML event methods<\/li>').append('<li>Effects and animations<\/li>')
 
 $('ul').before('<p>The jQuery library contains the following features:<\/p>');
+
+
+let html = $('html');
+
+html.css({'background-color': 'lightgrey'});
+
+title.css({'font-family': 'Helvetica, serif'});
+
+
+$('li').css({'font-family': 'Arial, serif',
+                    'color': 'blue'});
