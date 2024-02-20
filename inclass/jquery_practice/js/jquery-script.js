312ce653893211bb19443b2650d49ed1f2f81fbd
Michael Felix <mikemh@uri.edu>
2024-02-20 10:27:31 -0500

    jquery example

---
+++ inclass/jquery_practice/js/jquery-script.js
@@ -0,0 +1,11 @@
+let title = $('h1');
+
+title.html('<i>My jQuery Example<\/i>');
+
+title.append('<h1>jQuery is a JavaScript Library.<br>jQuery greatly simplifies JavaScript programming.<br>jQuery is easy to learn.<\/h1>');
+
+
+title.append('<ul><\/ul>')
+$('ul').append('<li>HTML/DOM manipulation<\/li>').append('<li>CSS manipulation<\/li>').append('<li>HTML event methods<\/li>').append('<li>Effects and animations<\/li>')
+
+$('ul').before('<p>The jQuery library contains the following features:<\/p>');
