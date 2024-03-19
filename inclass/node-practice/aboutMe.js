6fee77c449b268b91d26edec16ac632ab6c87588
Michael Felix <mikemh@uri.edu>
2024-03-19 10:31:04 -0400

    about me routes

--- inclass/node-practice/aboutMe.js
+++ inclass/node-practice/aboutMe.js
@@ -1,4 +1,4 @@
-var body = "<h1>Michael Miele-Herndon</h1>\n" +
+var index_body = "<h1>Michael Miele-Herndon</h1>\n" +
     "<p>I am a senior at URI majoring in Computer Science from the great state of New York. In my free time I enjoy reading, hiking, and playing video games.</p>" +
     "<h3>Favorite Movies & Books</h3>" +
     "<ol>" +
@@ -7,15 +7,36 @@ var body = "<h1>Michael Miele-Herndon</h1>\n" +
     "<li>Star Wars Original Trilogy</li>" +
     "</ol>"
 
+
+var routes = {
+    "/": function index(request, response) {
+        response.writeHead(200, {
+            'Content-Type': 'text/html',
+        });
+        response.end(index_body);
+    },
+    "/about": function index(request, response) {
+        response.writeHead(200, {
+            'Content-Type': 'text/html',
+        });
+        response.end("<h1>About Us</h1>");
+    },
+    "/contact": function index(request, response) {
+        response.writeHead(200, {
+            'Content-Type': 'text/html',
+        });
+        response.end("<h1>Contact Us</h1>");
+    },
+}
+
 var http = require('http');
 var port = 1337;
-
 http.createServer((request, response) => {
-    response.writeHead(200, {
-        'Content-Type': 'text/html',
-    });
-    response.write(body);
-    response.end();
+    if (request.url in routes) {
+        return routes[request.url](request, response);
+    } else {
+        response.end("<h1>404 Not Found</h1>");
+    }
 }).listen(port);
 
 console.log(`Listening... Go to http://localhost:${port}`);
