18924c1b84e133f49cad265d2616662192697501
Michael Felix <mikemh@uri.edu>
2024-03-19 10:40:12 -0400

    begin static file serv

--- inclass/node-practice/aboutMe.js
+++ inclass/node-practice/aboutMe.js
@@ -1,3 +1,7 @@
+var http = require('http');
+var fs = require('fs');
+
+
 var index_body = "<h1>Michael Miele-Herndon</h1>\n" +
     "<p>I am a senior at URI majoring in Computer Science from the great state of New York. In my free time I enjoy reading, hiking, and playing video games.</p>" +
     "<h3>Favorite Movies & Books</h3>" +
@@ -7,6 +11,25 @@ var index_body = "<h1>Michael Miele-Herndon</h1>\n" +
     "<li>Star Wars Original Trilogy</li>" +
     "</ol>"
 
+function serveStaticFile(response, path, contentType, status) {
+    if (!status) {
+        status = 200;
+    }
+
+    fs.readFile(__dirname + path, function(err, data) {
+        if (err) {
+            response.writeHead(500, {
+                'Content-Type': 'text/html',
+            });
+            response.end("<h1>Internal Server Error</h1>");
+        } else {
+            response.writeHead(status, {
+                'Content-Type': contentType,
+            });
+            response.end(data);
+        }
+    });
+}
 
 var routes = {
     "/": function index(request, response) {
@@ -29,16 +52,11 @@ var routes = {
     },
 }
 
-var http = require('http');
 var port = 1337;
 http.createServer((request, response) => {
-    if (request.url in routes) {
-        return routes[request.url](request, response);
-    } else {
-        response.writeHead(404, {
-            'Content-Type': 'text/html',
-        });
-        response.end("<h1>404 Not Found</h1>");
+    let path = request.url.replace('/\/?(?:\?.*)?$/', '');
+    switch (path) {
+
     }
 }).listen(port);
 
