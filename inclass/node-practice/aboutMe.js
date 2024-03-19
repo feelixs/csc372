9b9664b2cc6475b25774130939cb05fdb7ee24b8
Michael Felix <mikemh@uri.edu>
2024-03-19 10:14:38 -0400

    Update aboutMe.js

--- inclass/node-practice/aboutMe.js
+++ inclass/node-practice/aboutMe.js
@@ -1,11 +1,11 @@
 var body = "<h1>Michael Miele-Herndon</h1>\n" +
     "<p>I am a senior at URI majoring in Computer Science from the great state of New York. In my free time I enjoy reading, hiking, and playing video games.</p>" +
     "<h3>Favorite Movies & Books</h3>" +
-    "<ul>" +
-    "<li>Star Wars Original Trilogy</li>" +
+    "<ol>" +
     "<li>Eragon (Inheritence) Trilogy</li>" +
     "<li>The Lord of the Rings Series</li>" +
-    "</ul>"
+    "<li>Star Wars Original Trilogy</li>" +
+    "</ol>"
 
 var http = require('http');
 var port = 1337;
