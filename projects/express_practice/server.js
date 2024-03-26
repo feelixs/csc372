cf2abedfed8ce92e4619de37dc0afb70b6f4b065
Michael Felix <mikemh@uri.edu>
2024-03-26 10:05:34 -0400

    test express site

--- projects/express_practice/server.js
+++ projects/express_practice/server.js
@@ -5,9 +5,20 @@ const port = 1337;
 
 app.use(express.static('static'));
 
-
 app.get('/', function(req, res) {
-    res.sendFile('index.html');
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/dolphin', function(req, res) {
+    res.sendFile(__dirname + '/dolphin.html');
+});
+
+app.get('/unicorn', function(req, res) {
+    res.sendFile(__dirname + '/unicorn.html');
+});
+
+app.get('/*', function(req, res) {
+    res.sendFile(__dirname + '/404.html');
 });
 
 app.listen(port, () => {
