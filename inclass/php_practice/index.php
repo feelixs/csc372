6808bca74de36d88fea1dd510c449c289e4ce9ea
Michael Felix <mikemh@uri.edu>
2024-04-02 10:11:38 -0400

    php practic

---
+++ inclass/php_practice/index.php
@@ -0,0 +1,8 @@
+<!doctype html>
+<html>
+
+<head>
+    <title><?php echo "about me" ?></title>
+</head>
+
+</html>
---
+++ inclass/php_practice/variables.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<?php
+
+$name = "Michael";
+$year = 2024;
+$birth = 2001;
+
+?>
+
+<head>
+    <title>Age</title>
+</head>
+<body>
+<h1>Hello, <?php echo $name ?>.</h1>
+<h1>I am <?php echo $year - $birth ?> years old</h1>
+</body>
+</html>
